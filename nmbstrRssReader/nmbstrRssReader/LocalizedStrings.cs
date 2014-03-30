bc5e89ba54471c1eb63874e5d67760235069039a
Artem.Flotsky <wixfury@gmail.com>
2014-03-30 18:51:44 +0400

    created project

---
+++ nmbstrRssReader/nmbstrRssReader/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using nmbstrRssReader.Resources;
+
+namespace nmbstrRssReader
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
